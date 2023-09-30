20575183beca9fe135b2cd7429ca004080b393d4
CemWebDev <137628926+CemWebDev@users.noreply.github.com>
2023-09-30 14:46:33 +0300

    Add files via upload

---
+++ Dark And Light Mode Toggle - Javascript/script.js
@@ -0,0 +1,29 @@
+const toggleButton = document.querySelector("#toggle-button");
+const body = document.querySelector("body");
+const heading = document.querySelector("#header");
+const attribute = document.querySelector("#attribute");
+
+const Theme = (
+  bodyBgColor,
+  bodyTextColor,
+  themeHeader,
+  headerColor,
+  toggleButtonColor,
+  attributeColor
+) => {
+  body.style.backgroundColor = bodyBgColor;
+  body.style.color = bodyTextColor;
+  heading.innerHTML = themeHeader;
+  heading.style.color = headerColor;
+  toggleButton.style.color = toggleButtonColor;
+  attribute.style.color = attributeColor;
+};
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("fa-moon");
+  if (toggleButton.classList.toggle("fa-sun")) {
+    Theme("#fff", "#333", "Light Theme", "#333", "#333", "#333");
+  } else {
+    Theme("#333", "#fff", "Dark Theme", "#fff", "#fff", "#fff");
+  }
+});
