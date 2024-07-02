9e3eda6b8b9fe118158c7f6cfb3b1eae2f94b4b6
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-02 12:07:25 +0300

    new commit

--- 10-login-form/index.js
+++ 10-login-form/index.js
@@ -7,41 +7,38 @@ const form = document.getElementById("form");
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    validateInput();
-})
+  e.preventDefault();
+  validateInput();
+});
 
 const validateInput = () => {
-    if (nameInput.value.length >= 3) {
-        nameInput.classList.add("success");
-    } else {
-        nameInput.classList.remove("success");
-        nameInput.classList.add("error")
-    }
+  if (nameInput.value.length >= 3) {
+    nameInput.classList.add("success");
+  } else {
+    nameInput.classList.remove("success");
+    nameInput.classList.add("error");
+  }
 
-    if (email.value.match(emailFormat)) {
-        email.classList.add("success");
-    } else {
-        email.classList.remove("success");
-        email.classList.add("error");        
-    }
+  if (email.value.match(emailFormat)) {
+    email.classList.add("success");
+  } else {
+    email.classList.remove("success");
+    email.classList.add("error");
+  }
 
-
-    if (password.value.length >= 8) {
-        password.classList.add("success");
-    } else {
-        password.classList.remove("success");
-        password.classList.add("error");
-    }
-} 
-
+  if (password.value.length >= 8) {
+    password.classList.add("success");
+  } else {
+    password.classList.remove("success");
+    password.classList.add("error");
+  }
+};
 
 toggleBtn.addEventListener("click", () => {
-    toggleBtn.classList.toggle("fa-moon");
-    if (toggleBtn.classList.contains("fa-moon")) {
-        body.style.backgroundColor = "#333";
-    }
-    else {
-        body.style.backgroundColor = "#a2a8d3";
-    }
-})
+  toggleBtn.classList.toggle("fa-moon");
+  if (toggleBtn.classList.contains("fa-moon")) {
+    body.style.backgroundColor = "#333";
+  } else {
+    body.style.backgroundColor = "#a2a8d3";
+  }
+});
