c283d9b9273b4fc2c13ca252e316c8686a0bc274
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-08 17:33:54 +0300

    new commit

--- 07-instagram-sidebar/index.js
+++ 07-instagram-sidebar/index.js
@@ -2,4 +2,13 @@ const moreModalIcon = document.querySelector(".more i");
 const moreModalBox = document.querySelector(".more-modal-box");
 
 moreModalIcon.addEventListener("click", () => {
-})
+  moreModalBox.classList.toggle("visible");
+});
+window.addEventListener("click", (e) => {
+  if (
+    !e.composedPath().includes(moreModalIcon) &&
+    !e.composedPath().includes(moreModalBox)
+  ) {
+    moreModalBox.classList.remove("visible");
+  }
+});
