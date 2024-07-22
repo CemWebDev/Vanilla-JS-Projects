d38889594a9e8279e950b21e0b880ea7bfddf848
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-22 20:40:36 +0300

    new commit

--- 14-quiz-app/index.js
+++ 14-quiz-app/index.js
@@ -1,41 +1,4 @@
-const questions = [
-  {
-    question: "Which is the most used programming language in 2023?",
-    answers: [
-      { option: "Java", correct: false },
-      { option: "Python", correct: false },
-      { option: "Javascript", correct: true },
-      { option: "PHP", correct: false },
-    ],
-  },
-  {
-    question: "Which is the capital city of Türkiye?",
-    answers: [
-      { option: "Ankara", correct: true },
-      { option: "Antalya", correct: false },
-      { option: "Batman", correct: false },
-      { option: "Adana", correct: false },
-    ],
-  },
-  {
-    question: "Which is the traditional dish of Türkiye?",
-    answers: [
-      { option: "Paella", correct: false },
-      { option: "Pasta", correct: false },
-      { option: "Risotto", correct: false },
-      { option: "Kebap", correct: true },
-    ],
-  },
-  {
-    question: "How many states are there in the U.S?",
-    answers: [
-      { option: 234, correct: false },
-      { option: 12, correct: false },
-      { option: 5, correct: false },
-      { option: 50, correct: true },
-    ],
-  },
-];
+import { questions } from "./questions.js";
 
 const questionElement = document.querySelector(".question h3");
 const answerButtons = document.querySelector(".options");
@@ -44,12 +7,12 @@ const nextButton = document.querySelector("#next-button");
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz() {
+const startQuiz = () => {
   currentQuestionIndex = 0;
   score = 0;
   nextButton.innerHTML = "Next";
   showQuestion();
-}
+};
 
 function showQuestion() {
   resetPage();
---
+++ 14-quiz-app/questions.js
@@ -0,0 +1,39 @@
+export const questions = [
+    {
+      question: "Which is the most used programming language in 2023?",
+      answers: [
+        { option: "Java", correct: false },
+        { option: "Python", correct: false },
+        { option: "Javascript", correct: true },
+        { option: "PHP", correct: false },
+      ],
+    },
+    {
+      question: "Which is the capital city of Türkiye?",
+      answers: [
+        { option: "Ankara", correct: true },
+        { option: "Antalya", correct: false },
+        { option: "Batman", correct: false },
+        { option: "Adana", correct: false },
+      ],
+    },
+    {
+      question: "Which is the traditional dish of Türkiye?",
+      answers: [
+        { option: "Paella", correct: false },
+        { option: "Pasta", correct: false },
+        { option: "Risotto", correct: false },
+        { option: "Kebap", correct: true },
+      ],
+    },
+    {
+      question: "How many states are there in the U.S?",
+      answers: [
+        { option: 234, correct: false },
+        { option: 12, correct: false },
+        { option: 5, correct: false },
+        { option: 50, correct: true },
+      ],
+    },
+  ];
+  
